a6c0de5dd3e97eccddb6f7a5a11f0944c583ecc3
tuan <tuanpham22790@gmail.com>
2021-01-07 12:18:14 +0700

    buoi17

---
+++ Buoi17/alert.js
@@ -0,0 +1,20 @@
+// Tạo một nút click để hiển thị thông báo trên góc trình duyệt:
+
+// - sau 2s tự động tắt
+
+// - hoặc có nút tắt trong bảng thông báo
+
+// Bonus: Có thể bấm nhiều thông báo
+
+let alert = document.querySelector('.alert');
+let ulAlert = document.querySelector('.ul-alert');
+let button = document.querySelector('.button');
+
+button.onclick = function () {
+    let li = document.createElement("li");
+    li.innerHTML = "Alert";
+    ulAlert.append(li);
+    let time = setInterval(function () {
+        ulAlert.removeChild(li)
+    },2000)
+}
